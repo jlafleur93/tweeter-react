fb8a050e5fc9f5eceeb5ecbd5a64b39f0ad3a005
joelwowz <josephlafleur@hotmail.com>
2021-11-28 14:38:37 -0700

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
-import plus from './assets/plus-sign.svg'
-import message from './assets/message.svg'
 function App() {
+  const userDets = {
+    user: "Snoop Dog",
+    totalPosts: "10",
+    posts: {
+      p01: {
+        id: 1,
+        message: "yeet",
+      },
+      p02: {
+        id: 2,
+        message: "same",
+      },
+    },
+  };
   return (
     <body>
-      <Nav/>
+      <Nav info={userDets.user} />
     </body>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./Nav.css";
-import plus from "../assets/plus-sign.svg"
-import message from "../assets/message.svg"
-import profile from "../assets/profile.svg"
+import plus from "../assets/plus-sign.svg";
+import message from "../assets/message.svg";
+import profile from "../assets/profile.svg";
 
 export default function Nav(props) {
-  const {image} = props
+  const { info } = props;
   return (
     <nav className="tweet-bar">
-      <img src={profile}/>
+      <div className="profile-info">
+        <img className="profile-pic" src={profile} />
+        <p>{info}</p>
+      </div>
       <span>tweeter</span>
-      <div  className="write-tweet">
+      <div className="write-tweet">
         <span>
-          <strong>Write</strong> a new tweet. 
+          <strong>Create</strong> a new post.
         </span>
-        <img src={plus}/>
-        <img src={message}/>
+        <img src={plus} />
+        <img src={message} />
         <div class="container"></div>
       </div>
-      
     </nav>
   );
 }
